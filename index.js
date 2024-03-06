1332d11328e277243de85eb2153b249e29b10580
Alan Jone Exterkortter <alanjoneexter@outlook.com>
2024-03-06 10:36:28 -0300

    Versão Inicial

---
+++ index.js
@@ -0,0 +1,27 @@
+var setadireita = window.document.getElementById("setadireita")
+var leonardo = window.document.getElementById("leonardo")
+var samantha = window.document.getElementById("samantha")
+var bruna = window.document.getElementById("bruna")
+var setaesquerda = window.document.getElementById("setaesquerda")
+
+
+function rolardireita(){
+
+    leonardo.style ="display:none"
+    bruna.style ="display:flex"
+    setadireita.style ="display:none"
+    setaesquerda.style ="display:flex; margin-top:55px; margin-left:50px;"
+}
+
+function rolaresquerda(){
+    leonardo.style ="display:flex"
+    bruna.style ="display:none"
+    setadireita.style ="display:flex; margin-top:55px; margin-left:50px "
+    setaesquerda.style ="display:none"
+
+}
+
+
+
+
+
